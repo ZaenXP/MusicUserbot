99d4daabccf7d6eeab94a5d1ad3defd0c3ff2e8d
ZaenXp <107419846+ZaenXP@users.noreply.github.com>
2022-08-03 15:15:03 +0700

    ;(

---
+++ MusicUserbot/help.py
@@ -0,0 +1,36 @@
+# Zaen [@TeleUdahRusak
+# Don't remove credits ⚠️
+
+import os
+import sys
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from config import HNDLR, SUDO_USERS
+
+
+@Client.on_message(filters.command(["help"], prefixes=f"{HNDLR}"))
+async def help(client, m: Message):
+    await m.delete()
+    HELP = f"""
+<b>👋 Hallo {m.from_user.mention}!
+
+🛠 MENU BANTUAN MUSIC PLAYER
+
+⚡ PERINTAH UNTUK SEMUA ORANG
+• {HNDLR}play [judul lagu | link youtube | balas file audio] - untuk memutar lagu
+• {HNDLR}videoplay [judul video | link youtube | balas file video] - untuk memutar video
+• {HNDLR}playlist untuk melihat daftar putar
+• {HNDLR}ping - untuk cek status
+• {HNDLR}id - untuk melihat id pengguna
+• {HNDLR}video - judul video | link yt untuk mencari video
+• {HNDLR}song - judul lagu | link yt untuk mencari lagu
+• {HNDLR}help - untuk melihat daftar perintah
+• {HNDLR}join- untuk join | ke grup 
+
+⚡ PERINTAH UNTUK SEMUA ADMIN
+• {HNDLR}resume - untuk melanjutkan pemutaran lagu atau video
+• {HNDLR}pause - untuk untuk menjeda pemutaran lagu atau video
+• {HNDLR}skip - untuk melewati lagu atau video
+• {HNDLR}end - untuk mengakhiri pemutaran</b>
+"""
+    await m.reply(HELP)
