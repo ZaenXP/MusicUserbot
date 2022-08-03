119811c9082a56d9e4d0aa4c7114d4efbf74341d
ZaenXp <107419846+ZaenXP@users.noreply.github.com>
2022-08-03 15:24:21 +0700

    Fix

---
+++ MusicUserbot/ping.py
@@ -0,0 +1,77 @@
+import os
+import sys
+from datetime import datetime
+from time import time
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from config import HNDLR, SUDO_USERS
+
+# System Uptime
+START_TIME = datetime.utcnow()
+TIME_DURATION_UNITS = (
+    ("Minggu", 60 * 60 * 24 * 7),
+    ("Hari", 60 * 60 * 24),
+    ("Jam", 60 * 60),
+    ("Menit", 60),
+    ("Detik", 1),
+)
+
+
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return "inf"
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append("{} {}{}".format(amount, unit, "" if amount == 1 else ""))
+    return ", ".join(parts)
+
+@Client.on_message(filters.command(["ping"], prefixes=f"{HNDLR}"))
+async def ping(client, m: Message):
+   start = time()
+   current_time = datetime.utcnow()
+   m_reply = await m.edit("Pinging ngewe...")
+   delta_ping = time() - start
+   await m_reply.edit("0% ▒▒▒▒▒▒▒▒▒▒")
+   await m_reply.edit("20% ██▒▒▒▒▒▒▒▒")
+   await m_reply.edit("40% ████▒▒▒▒▒▒")
+   await m_reply.edit("60% ██████▒▒▒▒")
+   await m_reply.edit("80% ████████▒▒")
+   await m_reply.edit("100% ██████████")
+   end = datetime.now()
+   uptime_sec = (current_time - START_TIME).total_seconds()
+   uptime = await _human_time_duration(int(uptime_sec))
+   await m_reply.edit(f"**┞◈𝗣𝗼𝗻𝗴!!🏓**\n**┞◈Pinger**  - {delta_ping * 1000:.3f} ms \n**┞◈Uptime** - {uptime}")
+
+
+@Client.on_message(filters.command(["pong"], prefixes=f"{HNDLR}"))
+async def pong(client, m: Message):
+   start = time()
+   current_time = datetime.utcnow()
+   pong = await m.edit("kontol jamet...")
+   delta_ping = time() - start
+   await pong.edit("❏◈===❏")
+   await pong.edit("❏=◈==❏")
+   await pong.edit("❏==◈=❏")
+   await pong.edit("❏===◈❏")
+   await pong.edit("❏==◈=❏")
+   await pong.edit("❏=◈==❏")
+   await pong.edit("❏◈===❏")
+   await pong.edit("❏=◈==❏")
+   await pong.edit("❏==◈=❏")
+   await pong.edit("❏===◈❏")
+   await pong.edit("❏==◈=❏")
+   await pong.edit("❏=◈==❏")
+   await pong.edit("❏◈===❏")
+   await pong.edit("❏=◈==❏")
+   await pong.edit("❏==◈=❏")
+   await pong.edit("❏===◈❏")
+   await pong.edit("❏===◈❏◈")
+   await pong.edit("❏====❏◈◈")
+   await pong.edit("**◈CROOTTTT PINGGGG!**")
+   end = datetime.now()
+   uptime_sec = (current_time - START_TIME).total_seconds()
+   uptime = await _human_time_duration(int(uptime_sec))
+   await pong.edit(
+       f"**❏MusicUserbot**\n**❏NGENTOT** : {delta_ping * 1000:.3f} ms\n**❏Bot Uptime** : {uptime}")
