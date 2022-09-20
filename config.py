d8510a947f9b30d783893756db884b8df9a112ca
ZaenXp <107419846+ZaenXP@users.noreply.github.com>
2022-09-20 16:55:58 +0700

    .

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 SESSION = os.getenv("SESSION")
 HNDLR = os.getenv("HNDLR", "!")
-SUDO_USERS = list(map(int, os.getenv("SUDO_USERS", "").split()))
+SUDO_USERS = list(map(int, os.getenv("SUDO_USERS").split()))
 
 
 contact_filter = filters.create(
